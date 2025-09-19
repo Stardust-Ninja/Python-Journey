f39072d222537481d7d6a1250e5d02d321fe1c2f
Stardust-Ninja <168665635+Stardust-Ninja@users.noreply.github.com>
2025-09-19 11:37:15 +0200

    Update 1. Rock Paper Scissors.py
    turned it into English

--- 1. Rock Paper Scissors.py
+++ 1. Rock Paper Scissors.py
@@ -1,16 +1,17 @@
 import random
 player = input ("wat is je keuze: ")
-keuze = ["steen", "papier", "schaar"]
-computer = random.choice(keuze)
+choice= ["rock", "paper", "scissors"]
+computer = random.choice(choice)
 print("computer kiest", computer)
 
 if player == computer:
-    print("gelijkspel")
+    print("draw")
 elif player == "steen" and computer == "schaar":
-    print("je wint")
+    print("you win")
 elif player == "papier" and computer == "steen":
-    print("je wint")
+    print("you win")
 elif player == "schaar" and computer == "papier":
-    print("je wint")
+    print("you win")
 else:
-    print("computer wint")
+    print("computer wins")
+
